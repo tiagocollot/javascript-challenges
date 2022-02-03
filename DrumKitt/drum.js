1eec5dc5660e4ae4a5733633bd70dd4117756a5e
Tiago Collot <collot.tiago1@gmail.com>
2022-02-03 21:08:46 +0000

    Added js path

--- DrumKitt/drum.js
+++ DrumKitt/drum.js
@@ -1,19 +1,19 @@
-// function playSound(e) {
-//   const audio = document.querySelector(`audio[data-key="${e.keyCode}] "`)
-//   const key = document.querySelector(`.key[data-key="${e.keyCode}] "`)
-//   if (!audio) return; // stop the fuction running all together
-//   audio.currentTime = 0; // rewind to the start
-//   audio.play();
-//   console.log(key);
-//   key.classList.add("playing ");
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}] "`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}] "`)
+  if (!audio) return; // stop the fuction running all together
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  console.log(key);
+  key.classList.add("playing ");
 
-// }
+}
 
-// function removeTransition(e) {
-//   if (e.propertyName !== 'transform') return; // skip it if is not a Transform
-//   this.classList.remove('playing');
-// };
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // skip it if is not a Transform
+  this.classList.remove('playing');
+};
 
-// const keys = document.querySelectorAll('.key');
-// keys.forEach(key => add.addEventListener('transitionend', removeTransition));
-// window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => add.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
