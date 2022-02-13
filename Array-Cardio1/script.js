46541ea417cd53ef7b0d70a5d81c28cbeb03ac58
Tiago Collot <collot.tiago1@gmail.com>
2022-02-13 15:52:14 +0000

    identing

--- Array-Cardio1/script.js
+++ Array-Cardio1/script.js
@@ -1,5 +1,17 @@
 const inventors = [
-  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 }; { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 }; { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 }; { first: 'Marie', last: 'Curie', year: 1867, passed: 1937 }; { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }; { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 }; { first: 'Max', last: 'Plank', year: 1858, passed: 1947 };
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 };
+
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 };
+
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 };
+
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1937 };
+
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 };
+
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 };
+
+  { first: 'Max', last: 'Plank', year: 1858, passed: 1947 };
 
 ]
 
