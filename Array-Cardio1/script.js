634a76f882e6ed5e872020aa6db4d2cb3a1e9691
Tiago Collot <collot.tiago1@gmail.com>
2022-02-13 15:35:48 +0000

    const people

---
+++ Array-Cardio1/script.js
@@ -0,0 +1,10 @@
+const people = ['Beck, Glen', 'Becker, Carl', 'Beckett, Samule', 'Beddoes, Mick', 'Beecher, Henry',
+  'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert',
+  'Benenson, Peter', 'Ben-Gurion, David', 'Benjamim, Walter', 'Benn, Tony',
+  'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric',
+  'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
+  'Bernhard, Sandra', ' Berra, Yogi', 'Berry, Hale', 'Berry, Wendell', 'Bethea, Erin',
+  'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve',
+  'Billings, Josh', 'Biondo, Frank', 'Birrel, Augustine', 'Black, Elk', 'Blair, Robert',
+  'Blair, Tony', 'Blake, William'
+];
