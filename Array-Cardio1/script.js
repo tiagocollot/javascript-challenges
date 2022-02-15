3fcd559db4ca1e412eef0a392f18c0bad619dad2
Tiago Collot <collot.tiago1@gmail.com>
2022-02-15 18:55:13 +0000

    Sort inventors by years lived

--- Array-Cardio1/script.js
+++ Array-Cardio1/script.js
@@ -1,33 +1,29 @@
-noconst inventors = [
-  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 };
+// const people = ['Beck, Glen', 'Becker, Carl', 'Beckett, Samule', 'Beddoes, Mick', 'Beecher, Henry',
+//   'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert',
+//   'Benenson, Peter', 'Ben-Gurion, David', 'Benjamim, Walter', 'Benn, Tony',
+//   'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric',
+//   'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
+//   'Bernhard, Sandra', ' Berra, Yogi', 'Berry, Hale', 'Berry, Wendell', 'Bethea, Erin',
+//   'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve',
+//   'Billings, Josh', 'Biondo, Frank', 'Birrel, Augustine', 'Black, Elk', 'Blair, Robert',
+//   'Blair, Tony', 'Blake, William'
+// ];
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
 
-  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 };
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
 
-  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 };
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
 
-  { first: 'Marie', last: 'Curie', year: 1867, passed: 1937 };
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1937 },
 
-  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 };
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
 
-  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 };
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
 
-  { first: 'Max', last: 'Plank', year: 1858, passed: 1947 };
+  { first: 'Max', last: 'Plank', year: 1858, passed: 1947 },
 
 ];
-
-
-
-const people = ['Beck, Glen', 'Becker, Carl', 'Beckett, Samule', 'Beddoes, Mick', 'Beecher, Henry',
-  'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert',
-  'Benenson, Peter', 'Ben-Gurion, David', 'Benjamim, Walter', 'Benn, Tony',
-  'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric',
-  'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
-  'Bernhard, Sandra', ' Berra, Yogi', 'Berry, Hale', 'Berry, Wendell', 'Bethea, Erin',
-  'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve',
-  'Billings, Josh', 'Biondo, Frank', 'Birrel, Augustine', 'Black, Elk', 'Blair, Robert',
-  'Blair, Tony', 'Blake, William'
-];
-
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who where born in the 1500´ st
@@ -54,10 +50,21 @@ console.table(birthDate);
 // });
 
 // Array.prototype.reduce()
-
 // 4. How many years did all the inventors live
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort((a, b) => {
+  const lastGuy = a.passed - a.year
+  const nextGuy = b.passed - b.year
+  if (lastGuy > nextGuy) {
+    return -1;
+  } else {
+    return 1;
+  }
+});
 
 // 6. Create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https: //en.wikipedia.org/wiki/Category:Boulevards_In_Paris
