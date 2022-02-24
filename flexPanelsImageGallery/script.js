10f2dcb44b601b8b0d1c29edb61b5e1363c99404
Tiago Collot <collot.tiago1@gmail.com>
2022-02-24 19:47:09 +0000

    e.propertyName.includes

--- flexPanelsImageGallery/script.js
+++ flexPanelsImageGallery/script.js
@@ -1,7 +1,9 @@
-const panels = document.querySelectorAll('.panels');
+const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
+  console.log("hello");
   this.classList.toggle('open');
 }
+
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
