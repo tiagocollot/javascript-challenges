ec34dcdbe3c1adb2c98e9123bd54d51ff4a0d7c6
Tiago Collot <collot.tiago1@gmail.com>
2022-02-24 20:01:47 +0000

    Fixed font-size and transform

--- flexPanelsImageGallery/script.js
+++ flexPanelsImageGallery/script.js
@@ -1,9 +1,18 @@
-const panels = document.querySelectorAll('.panel');
+//  const panels = document.querySelectorAll('.panel');
 
-function toggleOpen() {
-  console.log("hello");
-  this.classList.toggle('open');
-}
+//     function toggleOpen() {
+//       console.log("hello");
+//       this.classList.toggle('open');
+//     }
+
+//     function toggleActive(e) {
+//       console.log(e.propertyName);
+//       if (e.propertyName.includes('flex')) {
+//         this.classList.toggle('open-active');
+
+//       }
+//     }
 
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+//     panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+//     panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
