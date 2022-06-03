2972b6ded5872286c87e7fdf44927312028693c2
Tiago Collot <collot.tiago1@gmail.com>
2022-06-03 23:03:10 +0100

    Created an array of objects

--- TeamStats/app.js
+++ TeamStats/app.js
@@ -1,3 +1,13 @@
 const teams = {
-
+  _players: [
+    { firstName: "Pete",
+     lastName: "Wheeler",
+     age: 54 },
+    { firstName: "Katrina",
+     lastName: "ler",
+     age: 54 },
+    { firstName: Te",
+     lastName: "Whee",
+     age: 54 },
+  _games: [],
 };
