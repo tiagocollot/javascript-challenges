36cb1e4c34b31179c2af755061fcd7d040e12010
Tiago Collot <collot.tiago1@gmail.com>
2022-06-03 16:57:20 +0100

    Added Setter

--- Project-Meal-Maker/app.js
+++ Project-Meal-Maker/app.js
@@ -1,7 +1,17 @@
+'use strict'
+
 const menu = {
  _meal: '',
  _price: 0
-}
+
+  set meal(mealToCheck){
+   if(typeof mealToCheck === 'string') {
+    return this.meal = mealToCheck;
+   }
+
+ }
+};
 
 menu._meal = 8;
-menu._price = "Pasta"
+menu._price = "Pasta";
+console.log(menu);
