56fb68ac1b3fcfa8c7790dd1f70aa2a601df0bd1
Tiago Collot <collot.tiago1@gmail.com>
2022-06-03 23:13:04 +0100

    Creates _games objects

--- TeamStats/app.js
+++ TeamStats/app.js
@@ -1,13 +1,12 @@
 const teams = {
   _players: [
-    { firstName: "Pete",
-     lastName: "Wheeler",
-     age: 4 },
-    { firstName: "Katrina",
-     lastName: "ler",
-     age: 5 },
-    { firstName: "Te",
-     lastName: "Whee",
-     age: 54 },
-  _games: [],
+    { firstName: "Pete", lastName: "Wheeler", age: 4 },
+    { firstName: "Katrina", lastName: "ler", age: 5 },
+    { firstName: "Te", lastName: "Whee", age: 54 },
+  ],
+
+  _games: [{
+    opponent: "skywalker",
+    teamPoints: 10,
+    opponentPoints: -7 }],
 };
