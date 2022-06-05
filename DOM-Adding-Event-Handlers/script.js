d9321927815af6d60c70d4f03ae9f067e1496273
Tiago Collot <collot.tiago1@gmail.com>
2022-06-05 16:34:50 +0100

    Adding Event Handler files created

---
+++ DOM-Adding-Event-Handlers/script.js
@@ -0,0 +1,18 @@
+let view = document.getElementById('view-button');
+let close = document.getElementById('close-button');
+let codey = document.getElementById('codey');
+
+let open = function() {
+  codey.style.display = 'block';
+  close.style.display = 'block';
+};
+
+let hide = function() {
+  codey.style.display = 'none';
+  close.style.display = 'none';
+};
+
+view.addEventListener('click', open);
+close.addEventListener('click', hide);
+
+// Write your code here
