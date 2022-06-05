0d2e1bb6b47accd64f8294364d56112154fb61a4
Tiago Collot <collot.tiago1@gmail.com>
2022-06-05 15:07:26 +0100

    Function onclick button red

--- DOM-Add-Click-Interactivity/script.js
+++ DOM-Add-Click-Interactivity/script.js
@@ -2,5 +2,9 @@ let element = document.querySelector('button');
 
 function turnButtonRed(){
   // Add code to turn button red
-
+  element.style.backgroundColor = 'red';
+  element.style.color = 'purple';
+  element.innerHTML = 'Red Button';
 }
+
+element.onclick = turnButtonRed
