2ddfea48a74ff6e71826bebaeddb8f2befcf60ac
Tiago Collot <collot.tiago1@gmail.com>
2022-06-05 18:36:45 +0100

    Created files

---
+++ DOM/8.DOM-Event-Object-Properties/script.js
@@ -0,0 +1,8 @@
+let social = document.getElementById('social-media');
+let share = document.getElementById('share-button');
+let text = document.getElementById('text');
+
+// Write your code below
+let sharePhoto = function(event) {
+
+}
