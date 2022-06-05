3da38939667107ee57762a801769f7a916bcdbac
Tiago Collot <collot.tiago1@gmail.com>
2022-06-05 18:56:07 +0100

    Created an object funtion and gave functionality with AddEventeLsitener

--- DOM/8.DOM-Event-Object-Properties/script.js
+++ DOM/8.DOM-Event-Object-Properties/script.js
@@ -5,4 +5,7 @@ let text = document.getElementById('text');
 // Write your code below
 let sharePhoto = function(event) {
 
+  event.target.style.display = "none";
+  text.innerHTML + event.timeStamp
 }
+share.addEventListener('click', sharePhoto);
