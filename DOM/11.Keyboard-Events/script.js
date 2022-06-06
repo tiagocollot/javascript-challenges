48bf14d4bc0faed7f8e7cefab9f2dadd5e8acf02
Tiago Collot <collot.tiago1@gmail.com>
2022-06-06 09:29:42 +0100

    Created keyup and key down events

--- DOM/11.Keyboard-Events/script.js
+++ DOM/11.Keyboard-Events/script.js
@@ -1,3 +1,14 @@
 let ball = document.getElementById('float-circle');
 
-// Write your code below
+// my code below
+let up = function() {
+  ball.style.bottom = '250px';
+}
+
+document.addEventListener("keydown", up);
+
+let down = function() {
+  ball.style.bottom = "50px"
+}
+
+document.addEventListener("keyup", down);
